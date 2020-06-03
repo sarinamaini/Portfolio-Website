4f412b541b58d792cc08a848d8f6f8b23299cb55
sarinamaini <66388922+sarinamaini@users.noreply.github.com>
2020-06-03 17:22:33 -0400

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$from = 'From: TangledDemo'; 
+$to = 'paarasbhandari@gmail.com'; 
+$subject = 'Feedback';
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Your message has been sent!</p>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    }
+}
+
+?>
